2bb6efbba588ace71babfd4a01e1673664776151
Michael Pollmeier <michael@michaelpollmeier.com>
2015-08-28 11:04:46 +1200

    test chats again, this time with freshly created users

--- src/test/scala/ylabs/messaging/ClientTest.scala
+++ src/test/scala/ylabs/messaging/ClientTest.scala
@@ -6,11 +6,9 @@ import akka.pattern.ask
 import akka.testkit.{ TestActorRef, TestProbe }
 import akka.util.Timeout
 import java.util.UUID
-import org.jivesoftware.smackx.iqregister.packet.Registration
 import org.scalatest.{ BeforeAndAfterEach, Matchers, WordSpec }
 import scala.collection.JavaConversions._
 import scala.concurrent.duration._
-import scala.util.Failure
 import scala.util.Success
 
 // this test depends on a running xmpp server (e.g. ejabberd) configured so that admin users can create unlimited users in your environment!
@@ -30,43 +28,52 @@ class ClientTest extends WordSpec with Matchers with BeforeAndAfterEach {
     adminUser ! Disconnect
   }
 
-  "user registration and deletion" in new Fixture {
-    val user = randomUsername
+  "allows user registration and deletion" in new Fixture {
+    val username = randomUsername
 
     adminUser ! Connect(adminUsername, adminPassword)
-    adminUser ! RegisterUser(user, password = user)
+    adminUser ! RegisterUser(username, password = username)
 
-    val connected1 = user1 ? Connect(user, password = user)
+    val connected1 = user1 ? Connect(username, password = username)
     connected1.value.get shouldBe Success(Connected)
 
     user1 ! DeleteUser
-    val connected2 = user1 ? Connect(user, password = user)
+    val connected2 = user1 ? Connect(username, password = username)
     connected2.value.get.get match {
-      case ConnectError(t) => //that's all we want to check
+      case ConnectError(t) ⇒ //that's all we want to check
     }
   }
 
-  // "chats to other users" in new Fixture {
-  //   val messageListener = TestProbe()
-  //   client2 ! RegisterMessageListener(messageListener.ref)
+  "enables users to chat to each other" in new Fixture {
+    val username1 = randomUsername
+    val username2 = randomUsername
 
-  //   client1 ! Connect(user1, pass1)
-  //   client2 ! Connect(user2, pass2)
-  //   client1 ! ChatTo(user2)
-  //   val testMessage = "test message"
-  //   client1 ! SendMessage(user2, testMessage)
+    adminUser ! Connect(adminUsername, adminPassword)
+    adminUser ! RegisterUser(username1, password = username1)
+    adminUser ! RegisterUser(username2, password = username2)
 
-  //   messageListener.expectMsgPF(3 seconds, "expected message to be delivered") {
-  //     case MessageReceived(chat, message) ⇒
-  //       chat.getParticipant shouldBe s"$user1@$domain/Smack"
-  //       message.getTo shouldBe s"$user2@$domain"
-  //       message.getBody shouldBe testMessage
-  //       true
-  //   }
+    user1 ! Connect(username1, password = username1)
+    user2 ! Connect(username2, password = username2)
 
-  //   client1 ! Disconnect
-  //   client2 ! Disconnect
-  // }
+    val messageListener = TestProbe()
+    user2 ! RegisterMessageListener(messageListener.ref)
+
+    user1 ! ChatTo(username2)
+    val testMessage = "unique test message" + UUID.randomUUID
+    user1 ! SendMessage(username2, testMessage)
+
+    messageListener.fishForMessage(3 seconds, "expected message to be delivered") {
+      case MessageReceived(chat, message) if !message.getBody.contains("Welcome") ⇒
+        chat.getParticipant should startWith(username1)
+        message.getTo should startWith(username2)
+        message.getBody shouldBe testMessage
+        true
+      case _ ⇒ false
+    }
+
+    user1 ! DeleteUser
+    user2 ! DeleteUser
+  }
 
   // "allows async chats" in new Fixture {
   //   val messageListener = TestProbe()
